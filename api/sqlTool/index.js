167b8e266eb84682c5d0b6451859a85080206384
ljuyi <981765731@qq.com>
2018-01-12 17:09:58 +0800

    添加注释

--- api/sqlTool/index.js
+++ api/sqlTool/index.js
@@ -4,6 +4,10 @@
 
 var mysql = require('mysql');
 var Connection
+
+/**
+ * 与数据库服务器建立连接
+ */
 exports.createConnection = function (host, user, password, dataBase) {
     Connection = mysql.createConnection({
         host: host,
@@ -15,22 +19,38 @@ exports.createConnection = function (host, user, password, dataBase) {
     return Connection;
 }
 
-exports.insertData = function (obj, database) {
-    Connection.query('insert ignore into ' + database + ' set ? ',
+/**
+ * 插入数据
+ * @param {object} obj - 插入的字段值
+ * @param {string} table - 待插入的数据表
+ */
+exports.insertData = function (obj, table) {
+    Connection.query('insert ignore into ' + table + ' set ? ',
         obj,
         function (error, results, fields) {
             if (error) throw error;
         })
 }
 
-exports.deleteData = function (conditions, database) {
-    Connection.query('delete from ' + database + ' where ' + conditions, function (error, results, fields) {
+/**
+ * 删除数据
+ * @param {json} conditions - 删除的条件
+ * @param {string} table - 待删除的数据表
+ */
+exports.deleteData = function (conditions, table) {
+    Connection.query('delete from ' + table + ' where ' + conditions, function (error, results, fields) {
         if (error) throw error;
         console.log('deleted ' + results.affectedRows + ' rows');
     })
 }
 
-exports.selectData = async function (field, conditions, database) {
+/**
+ * 提取数据
+ * @param {Array} field - 需要提取的字段
+ * @param {json} condtions - 提取的条件
+ * @param {string} table - 待提取的数据表
+ */
+exports.selectData = async function (field, conditions, table) {
     let result
     let string = ''
     for (let key in conditions) {
@@ -39,7 +59,7 @@ exports.selectData = async function (field, conditions, database) {
         string += key + '=' + "'" + conditions[key] + "'"
     }
     await new Promise((resolve, reject) => {
-        Connection.query('select ' + field.join(",") + ' from ' + database + ' where ' + string, function (error, results, fields) {
+        Connection.query('select ' + field.join(",") + ' from ' + table + ' where ' + string, function (error, results, fields) {
             if (error) throw error;
             resolve(results)
         })
@@ -49,10 +69,16 @@ exports.selectData = async function (field, conditions, database) {
     return result
 }
 
-exports.selectTopData = async function (type, num, database) {
+/**
+ * 从数据表中提取热门景点
+ * @param {string} type - 筛选热门景点的条件（根据评论数或者评分）
+ * @param {string} num - 需要的热门景点数目
+ * @param {string} table - 选择的数据表
+ */
+exports.selectTopData = async function (type, num, table) {
     let result
     await new Promise((resolve, reject) => {
-        let sql = "select name,std_tag,addr,area_name,overall_rating,image, link, short_desc, brief_ticket, mapsearchaladdin from " + database + " where std_tag like '%" + type + "%' order by comment_num desc limit 5 offset " + num
+        let sql = "select name,std_tag,addr,area_name,overall_rating,image, link, short_desc, brief_ticket, mapsearchaladdin from " + table + " where std_tag like '%" + type + "%' order by comment_num desc limit 5 offset " + num
         Connection.query(sql, function (error, results, fields) {
             if (error) throw error;
             resolve(results)
